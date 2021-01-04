98cec399fe4223b64579c2b15cc1d153ed667b3a
Bihari Zoltán <byhisoft@gmail.com>
2021-01-04 17:14:42 +0100

    Add application test classes

---
+++ ejapp/src/test/java/com/byhi/ejproject/ejapp/EjappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.byhi.ejproject.ejapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ ejdata/src/test/java/com/byhi/ejproject/ejdata/EjdataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.byhi.ejproject.ejdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjdataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
